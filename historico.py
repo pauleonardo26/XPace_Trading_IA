1a4faad67ca46714b9c9dbebb4e7aed6d32f39e7
pauleonardo26 <113878100+pauleonardo26@users.noreply.github.com>
2026-09-05 16:19:52 -0500

    Create obtener_historico function in historico.py
    Add a function to obtain historical data from OANDA.

---
+++ historico.py
@@ -0,0 +1,10 @@
+# ============================================================
+# XPACE TRADING IA - DATOS HISTÓRICOS
+# ============================================================
+
+def obtener_historico():
+    """
+    Aquí descargaremos posteriormente
+    los precios históricos desde OANDA.
+    """
+    pass
