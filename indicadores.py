126c6ad6ab382321f11609d9721d4141045cd6c9
pauleonardo26 <113878100+pauleonardo26@users.noreply.github.com>
2026-09-05 16:21:20 -0500

    Add calcular_indicadores function placeholder
    Added a function placeholder for calculating technical indicators.

--- indicadores.py
+++ indicadores.py
@@ -1,1 +1,10 @@
-q
+# ============================================================
+# XPACE TRADING IA - INDICADORES TÉCNICOS
+# ============================================================
+
+def calcular_indicadores():
+    """
+    Aquí calcularemos posteriormente
+    los indicadores técnicos del mercado.
+    """
+    pass
