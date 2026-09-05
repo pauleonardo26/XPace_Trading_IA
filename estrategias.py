db229d4d973da6b507283bf89cb36aa243e603e9
pauleonardo26 <113878100+pauleonardo26@users.noreply.github.com>
2026-09-05 16:23:23 -0500

    Create estrategias.py with evaluar_estrategia function
    Added a new file for trading strategies with a placeholder function.

---
+++ estrategias.py
@@ -0,0 +1,10 @@
+# ============================================================
+# XPACE TRADING IA - ESTRATEGIAS
+# ============================================================
+
+def evaluar_estrategia():
+    """
+    Aquí definiremos posteriormente
+    las reglas de nuestras estrategias.
+    """
+    pass
