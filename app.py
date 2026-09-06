d890f3287191d89b9a4ef5b8320881f2cd930587
pauleonardo26 <113878100+pauleonardo26@users.noreply.github.com>
2026-09-06 05:06:27 -0500

    Rename import from ejecutar_backtest to ejecutar_backtesting

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import pandas as pd
 from historico import obtener_historico
 from indicadores import calcular_indicadores
 from estrategias import generar_senales
-from backtesting import ejecutar_backtest
+from backtesting import ejecutar_backtesting
 from riesgo import calcular_riesgo_operacion
 from ia_analista import generar_informe_analista
 
